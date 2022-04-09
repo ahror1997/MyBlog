3e3e6ccb2ae2ec6f8ba00a1a952a063092350375
Ahror Ziyodinov <ziyodinov.ahror@loc.tashkent.uz>
2022-04-09 11:54:47 +0500

    finished category part

--- MyBlog/Controllers/Admin/CategoryController.cs
+++ MyBlog/Controllers/Admin/CategoryController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyBlog.Models;
+using System.Linq;
 
 namespace MyBlog.Controllers.Admin
 {
+    [Route("admin/[controller]")]
     public class CategoryController : Controller
     {
         private readonly ApplicationContext context;
@@ -12,14 +14,45 @@ namespace MyBlog.Controllers.Admin
             this.context = _context;
         }
 
+        [HttpGet]
+        [Route("[action]")]
         public IActionResult Index()
         {
-            return View("Views/Admin/Category/Index.cshtml");
+            var categories = context.Categories.ToList();
+            return View("Views/Admin/Category/Index.cshtml", categories);
         }
 
+        [HttpGet]
+        [Route("[action]")]
         public IActionResult Create()
         {
             return View("Views/Admin/Category/Create.cshtml");
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Create(Category category)
+        {
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("[action]/{Id}")]
+        public IActionResult Edit(int Id)
+        {
+            var category = context.Categories.Find(Id);
+            return View("Views/Admin/Category/Edit.cshtml", category);
+        }
+
+        [HttpPost]
+        [Route("[action]/{Id}")]
+        public IActionResult Edit(int Id, Category category)
+        {
+            context.Categories.Update(category);
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
